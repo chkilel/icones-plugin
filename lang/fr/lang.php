bac1e53cd82635a89f64f6314fce2e47b0a5a708
Adil Chehabi <adil@chehabi.ma>
2021-08-22 01:24:45 +0100

    Add French language

---
+++ lang/fr/lang.php
@@ -0,0 +1,34 @@
+<?php return [
+    "plugin" => [
+        "name" => "Icônes",
+        "description" => "Accédez à des milliers d'icônes, y compris les librairies d'icônes populaires, des polices d'icônes et plusieurs ensembles d'Emoji à partir d'un seul FormWidget backend",
+    ],
+    "formwidgets" => [
+        "placeholder" => "-- Choisir une icône --",
+        "error_wrong_variable_type" => "Le filtre Twig iconify doit être appliqué a une variable de type Icones",
+    ],
+    "settings" => [
+        "title" => "Icônes",
+        "label" => "Icônes",
+        "description" => "Gérez plus de 100.000 icônes vectorielles, y compris les librairies d'icônes populaires, des polices d'icônes et plusieurs librairies d'Emoji.",
+        "by_author" => "Par :name",
+        "enable_button" => "Activer",
+        "disable_button" => "Désactiver",
+        "delete_button" => "Supprimer",
+        "cant_delete_enabled" => "Vous ne pouvez pas supprimer les lirairies d'icônes actives, désactivez-les en premier.",
+        "delete_title" => "Les librairies d'icône actives ne peuvent pas être supprimées",
+        "delete_confirm" => "Êtes vous sûr de vouloir supprimer cette librairie?",
+        "install_button" => "Installer",
+        "flash_enabled" => "La librairie d'icônes est maintenant activées",
+        "flash_disabled" => "La librairie d'icônes est maintenant désactivées",
+        "flash_deleted" => "La librairie d'icônes est supprimées corrêctement",
+        "flash_installed" => "L'installation est terminées avec succès",
+        "flash_error_installation" => "Une ereur est survenue, la librairie d'icônes n'est pas installées",
+        "scroll_to_top" =>"Défiler vers le haut"
+    ],
+    "permission" => [
+        "manage_tab" => "Icônes",
+        "access_settings" => "Accéder aux paramètres d'Icônes",
+        "manage_installation" => "Gérer l'installation et la suppression des librairies d'icônes",
+    ],
+];
